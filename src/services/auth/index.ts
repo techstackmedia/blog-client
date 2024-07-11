2cf84c75eb1d2785bf71b53aa31da5e9487bed19
Osagie Bello <info@techstackmedia.com>
2024-07-11 12:55:13 +0100

    chore: remove unnecessary imports

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,7 +1,7 @@
 import api from '../api';
 
 export const login = async (email: string, password: string) => {
-  const response = await api.post('/auth/login', { email, password });
+  const response = await api.post('/api/auth/login', { email, password });
   return response.data;
 };
 
