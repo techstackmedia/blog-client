23dde5a3aecdacfcbe0c39ad7066feec6272471c
Osagie Bello <info@techstackmedia.com>
2024-07-11 16:36:49 +0100

    feat: adjust post editor

--- src/components/Posts/CreatePost/index.tsx
+++ src/components/Posts/CreatePost/index.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { usePosts } from '../../../context/Post';
 import { useUser } from '../../../context/User';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const CreatePost = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [isPreview, setIsPreview] = useState(false);
   const { createPost } = usePosts();
   const navigate = useNavigate();
   const { user } = useUser();
+  const name = user?.name;
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,33 +29,62 @@ const CreatePost = () => {
     }
   };
 
+  const handlePreviewMode = () => {
+    if (isPreview) {
+      setIsPreview(false);
+    } else {
+      setIsPreview(true);
+    }
+  };
+
   return (
     <form
       onSubmit={handleSubmit}
-      className='max-w-md mx-auto p-4 shadow-md rounded-md bg-white'
+      className='max-w-xl mx-auto p-4 shadow-md rounded-md bg-white'
     >
       <h2 className='text-2xl font-bold mb-4'>Create Post</h2>
-      <input
-        type='text'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder='Title'
-        required
-        className='w-full p-2 mb-4 border rounded'
-      />
-      <textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder='Content'
-        required
-        className='w-full p-2 mb-4 border rounded'
-      />
-      <button
-        type='submit'
-        className='w-full p-2 bg-purple-500 text-white rounded'
-      >
-        Create Post
-      </button>
+      {isPreview ? null : (
+        <input
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder='Title'
+          required
+          className='w-full p-2 mb-4 border rounded'
+        />
+      )}
+      {isPreview ? (
+        <div className='min-h-96 flex flex-col justify-between'>
+          <div>
+            <h1 className='text-3xl font-bold mb-4'>{title}</h1>
+            <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+          </div>
+          <p className='text-gray-600 my-5'>Author: {name}</p>
+        </div>
+      ) : (
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder='Content'
+          required
+          className='w-full p-2 mb-4 border rounded min-h-96 resize-none'
+        />
+      )}
+      <div className='flex gap-4'>
+        <button
+          type='submit'
+          className='w-full p-2 bg-purple-500 text-white rounded'
+        >
+          Create Post
+        </button>
+        <button
+          type='button'
+          className='w-full p-2 bg-blue-500 text-white rounded'
+          onClick={handlePreviewMode}
+        >
+          {isPreview ? 'Edit' : 'Preview'}
+        </button>
+      </div>
     </form>
   );
 };
